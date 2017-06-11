f111408c25a7982af21eb03b2f8d967cc17a44b9
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-11 16:18:50 +0200

    commit

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var pierwszyP = document.getElementById('pierwszy');
+console.log(pierwszyP);
+
+var drugiP = document.getElementById('drugi');
+console.log(drugiP);
+
+var input = document.getElementById('trzeci');
+console.log(input);
+
+function ustawTlo() {
+	console.log("dziala");
+	pierwszyP.style.backgroundColor = "red";
+	drugiP.style.backgroundColor = " yellow";
+}
+
+input.ondblclick = ustawTlo;
+	
